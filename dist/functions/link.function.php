de79b25e9b222bbab10b11d2e04d3ab75b478de2
Mehdi Kabab <mkabab@clever-age.com>
2018-07-04 14:50:42 +0200

    fix(functions): accept any HTML attribute for links

--- dist/functions/link.function.php
+++ dist/functions/link.function.php
@@ -2,13 +2,18 @@
 
 $function = new Twig_SimpleFunction(
     'link',
-    function ($title, $url, $attributes) {
-      if (isset($attributes) && isset($attributes['class'])) {
-        $classes = join(' ', $attributes['class']);
-        return '<a href="' . $url . '" class="' . $classes . '">' . $title . '</a>';
-      } else {
-        return '<a href="' . $url . '">' . $title . '</a>';
+    function ($title, $url, $attributes = []) {
+      $attributes['href'] = $url;
+      $attr = [];
+      foreach ($attributes as $key => $value) {
+        $attr[] = sprintf(
+          '%s="%s"',
+          $key,
+          is_array($value) ? implode(' ', $value) : $value
+        );
       }
+
+      return '<a ' . implode(' ', $attr) . '>' . $title . '</a>';
     },
     array('is_safe' => array('html'))
 );
